97fff0e634ad15e25b887deed587e355f93c235c
Phạm Trâm Anh <93437075+tramanhpham@users.noreply.github.com>
2024-03-15 08:38:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -24,3 +24,16 @@ if image is not None:
 
     st.header('Result')
     st.text(class_list[label])
+
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
